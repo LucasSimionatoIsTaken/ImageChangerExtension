9dcb48d89b8ff5c704285150755c8ac4da80ea7b
Lucas Simionato <sdfhjkasdfhsdkd@hfdjsfdsa>
2024-09-10 13:02:47 -0300

    versao 1

---
+++ main.js
@@ -0,0 +1,18 @@
+const images = [
+    "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+    "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_3x2.jpg",
+    "https://d3544la1u8djza.cloudfront.net/APHI/Blog/2021/07-06/small+white+fluffy+dog+smiling+at+the+camera+in+close-up-min.jpg",
+]
+
+function changeImgs() {
+    const imgEls = Array.from(document.querySelectorAll('img'))
+    const imgQtd = images.length
+
+    imgEls.forEach(imgEl => {
+        imgEl.src = images[Math.floor(Math.random() * imgQtd)]
+    })
+}
+
+setTimeout(() => {
+    setInterval(() => changeImgs(), 200)
+}, 4000)
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Image Maker
+
+Extensão que troca as imagens de uma página por imagens de cachorros.
+Você pode trocar as imagens colocando seus links na main.js no array images
+
+Para ativar a extensão, 
+1. Abra a página chrome://extensions
+2. Ative o modo de desenvolvedor
+3. Clique em carregar sem compactação e selecione a pasta
